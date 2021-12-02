2b0bef2cc28382e1c538d7aa88188c882616fb09
Sander Nijhof <sn@xprtz.net>
2021-12-02 09:18:20 +0100

    Day 2

---
+++ AdventOfCode/AdventOfCode2/Program.cs
@@ -0,0 +1,50 @@
+﻿var directions = File.ReadAllLines("./Input.txt").ToList();
+
+//Part1
+var horizontal = 0;
+var vertical = 0;
+
+
+foreach (string direction in directions)
+{
+    var command = direction.Split(' ');
+
+    var result = command[0] switch
+    {
+        "forward" => horizontal += int.Parse(command[1]),
+        "backward" => horizontal -= int.Parse(command[1]),
+        "up" => vertical -= int.Parse(command[1]),
+        "down" => vertical += int.Parse(command[1]),        
+        _ => throw new NotSupportedException()
+    };
+}
+
+Console.WriteLine(horizontal * vertical);
+
+//Part2
+var aim = 0;
+horizontal = 0;
+vertical = 0;
+foreach (string direction in directions)
+{
+    var command = direction.Split(' ');
+
+    switch (command[0])
+    {
+        case "forward":
+            horizontal += int.Parse(command[1]);
+            vertical += aim * int.Parse(command[1]);
+            break;
+        case "backward":
+            break;
+        case "down":
+            aim += int.Parse(command[1]);
+            break;
+        case "up":
+            aim -= int.Parse(command[1]);
+            break;
+
+    }
+}
+
+Console.WriteLine(horizontal * vertical);
